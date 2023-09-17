ee1111553fd0e9521d5682de98aa7cb7db880a9d
imchopi <apchopito@gmail.com>
2023-09-17 21:15:51 +0200

    test

--- src/Components/Descripcion.js
+++ src/Components/Descripcion.js
@@ -1,7 +1,7 @@
 import React from 'react';  
 
 const Descripcion = () => {
-    return <p>Esto es una descripción nueva</p>
+    return <p>Esto es una descripción nueva pero nueva eh</p>
 }
 
 export default Descripcion;
